4d89dcd40eabe557fe1708b9ea9291a86be2ca95
mspring-dartmouth <77127789+mspring-dartmouth@users.noreply.github.com>
2024-05-14 13:57:19 -0400

    The version 'fix' that I implemented in the last commit broke setup.cfg. This repairs that.

--- doricprocessingtoolbox/__init__.py
+++ doricprocessingtoolbox/__init__.py
@@ -1,7 +1,9 @@
+from ._Doric_Processing_Toolbox import __version__
+__version__ = __version__
+
 from ._Doric_Processing_Toolbox import (bidirectional_trap_sum,
                                        butter_lowpass_filter,
                                        calc_robust_z,
                                        RenamingUnpickler,
                                        sig_processing_object)
 
-from ._Doric_Processing_Toolbox import __version__
